3169ec7071b9ded058c313c59c3401719222c324
mattsu <mattsuyolo@gmail.com>
2020-09-06 17:58:00 +0800

    databasetest

---
+++ app/Http/Controllers/DataBaseTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class DataBaseTest extends Controller
+{
+    public function index()
+    {
+        $users = DB::select('select * from users');
+        var_dump($users);
+    }
+}
