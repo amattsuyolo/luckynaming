4070aef76f4e7271a6b208ae974799d1ec522094
mattsu <mattsuyolo@gmail.com>
2020-09-16 22:32:24 +0800

    調整表單為components

---
+++ resources/views/nameformal/components/form.blade.php
@@ -0,0 +1,29 @@
+<div class="col-lg-8 col-md-10 mx-auto">
+        <h5 class=" info-color white-text text-center py-4">
+          <strong>搜索吉祥美名</strong>
+        </h5>
+        <form style="background-image:url(nametest/img/butterfly2.jpg);background-repeat: no-repeat; background-size: 100% auto;">
+          <div class="form-group">
+            <label for="exampleInputEmail1">姓氏</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="請輸入姓氏">
+          </div>
+          <div class="form-group">
+            <label for="example-date-input" >生日</label>
+            <input class="form-control" type="date" value="2011-01-01" id="example-date-input">
+            <small id="emailHelp" class="form-text text-muted">請選擇西元生日</small>
+          </div>
+          <div>
+            <label for="example-date-input" >性別</label>
+            <div class="form-group">
+            <label class="radio-inline">
+                <input type="radio" name="optradio" checked>男
+            </label>
+            <label class="radio-inline">
+                <input type="radio" name="optradio">女
+            </label>
+         </div>
+        <div class="text-center">
+          <button type="submit" style="font-size:large;" class="btn btn-info  btn-block">立即獲取</button>
+        </div>
+        </form>
+</div>
