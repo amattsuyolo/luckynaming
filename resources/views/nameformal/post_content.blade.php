f468674070ecabee96289825dd951f9075b59c1c
mattsu <mattsuyolo@gmail.com>
2020-10-16 22:53:45 +0800

    新增文章標題文字寬度縮小

--- resources/views/nameformal/post_content.blade.php
+++ resources/views/nameformal/post_content.blade.php
@@ -18,7 +18,7 @@
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto" style="position:relative">
+        <div class="col-10 col-lg-8 col-md-10 mx-auto" style="position:relative">
           <div class="post-heading">
             <h1 id="post-title">{{ $title }}</h1>
             <h2 class="subheading">{{ $sub_title }}</h2>
