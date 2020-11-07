42970aaa9a8522f8aed51bfc77a3f8b3ac09677f
mattsu <mattsuyolo@gmail.com>
2020-11-07 17:00:18 +0800

    add post factory and seed

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -6,8 +6,20 @@ use App\Post;
 use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
+    // Random datetime of the current week
+    $startingDate = $faker->dateTimeBetween('this week', '+6 days');
+    // Random datetime of the current week *after* `$startingDate`
+    $endingDate   = $faker->dateTimeBetween($startingDate, strtotime('+6 days'));
+
     return [
+        'user_id' => factory(App\User::class),
         'title' => $faker->realText($maxNbChars = 15),
-        'body' => $faker->realText($maxNbChars = 150),
+        'min_pic' => $faker->imageUrl($width = 640, $height = 480),
+        'subtitle' => $faker->title,
+        'body' => $faker->paragraph,
+        'is_published' => 1,
+        'published_at' =>  $startingDate,
+        'published_until' =>$endingDate ,
     ];
 });
+
--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use App\Post;
 
 class PostsTableSeeder extends Seeder
 {
@@ -13,9 +14,15 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('posts')->insert([
-            'title' => Str::random(10),
-            'body' => Str::random(50),
-        ]);
+        DB::table('posts')->truncate();
+        Post::unguard();
+        factory(Post::class, 10)->create();
+        // DB::table('posts')->insert([
+        //     'title' => Str::random(10),
+        //     'subtitle' => Str::random(10),
+        //     'body' => Str::random(50),
+        //     'is_published' => 0
+        // ]);
+        Post::reguard();
     }
 }
