49f1ac598d3be825dd6ae75edd7676d120c8f688
mattsu <mattsuyolo@gmail.com>
2021-04-04 15:18:08 +0800

    新增各行解析欄位

---
+++ database/migrations/2021_04_04_071311_add_paragraph_analysis.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddParagraphAnalysis extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Signs', function (Blueprint $table) {
+            $table->string('first')->nullable()->after('analysis')->comment('第一句解析');
+            $table->string('second')->nullable()->after('first')->comment('第二句解析');
+            $table->string('third')->nullable()->after('second')->comment('第三句解析');
+            $table->string('fourth')->nullable()->after('third')->comment('第四句解析');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Sign', function (Blueprint $table) {
+            //
+        });
+    }
+}
