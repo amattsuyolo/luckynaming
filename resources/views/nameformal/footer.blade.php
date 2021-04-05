81f399abcf1a93fa396e1e2b7f2d84e383e77dd0
mattsu <mattsuyolo@gmail.com>
2021-04-05 16:46:59 +0800

    更改copy right 年份

--- resources/views/nameformal/footer.blade.php
+++ resources/views/nameformal/footer.blade.php
@@ -5,7 +5,7 @@
         <div class="col-lg-8 col-md-10 mx-auto" style="text-align: center;">
         <div class="line-it-button" data-lang="zh_Hant" data-type="share-a" data-ver="3" data-url="{{ url()->current() }}" data-color="default" data-size="large" data-count="false" style="display: none;"></div>
         <script src="https://d.line-scdn.net/r/web/social-plugin/js/thirdparty/loader.min.js" async="async" defer="defer"></script>   
-        <p class="copyright text-muted">Copyright &copy;2020 babyname. All rights reserved. 2020</p>
+        <p class="copyright text-muted">Copyright &copy;2021 babyname. All rights reserved. 2021</p>
         </div>
       </div>
     </div>
